21d3f5a07ace4eba3628572f4bdc5a8a5d8d4d0e
Kamil Mrowka <kamil@chicos.life>
2024-10-08 07:50:04 +0200

    Add helpers

---
+++ src/utils.js
@@ -0,0 +1,40 @@
+const toSnakeCase = str => {
+  return str
+    .replace(/\s+/g, '_')
+    .replace(/([A-Z])/g, '_$1')
+    .toLowerCase();
+};
+
+const toCamelCase = str => {
+  return str.replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+};
+
+export const convToSnake = obj => {
+  console.log(obj)
+  return Object.keys(obj).reduce((pv, cv) => {
+    pv[toSnakeCase(cv)] = obj[cv];
+    return pv;
+  }, {});
+};
+
+export const mapObjToSnake = (obj, keys) => {
+  return keys.reduce((pv, cv) => {
+    if (!obj[cv]) return pv;
+
+    pv[cv] = obj[cv];
+    return pv;
+  }, {});
+};
+
+export const mapToCamel = obj => {
+  return Object.keys(obj).reduce((pv, cv) => {
+    pv[toCamelCase(cv)] = obj[cv];
+    return pv;
+  }, {});
+};
+
+export const getFormatedDate = (now = new Date()) => {
+  const date = now.toISOString().split('T')[0]; // format YYYY-MM-DD
+  const time = now.toTimeString().split(' ')[0].slice(0, 5); // format HH:mm
+  return `${date} ${time}`;
+};
