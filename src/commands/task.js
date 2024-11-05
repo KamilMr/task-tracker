1f629d2d48da2811e59c895e820a927529550ee4
Kamil Mrowka <kamil@chicos.life>
2024-11-05 21:20:50 +0100

    Dirty draft

--- src/commands/task.js
+++ src/commands/task.js
@@ -4,8 +4,12 @@ import projectModel from '../models/project.js';
 import taskService from '../services/taskService.js';
 import {isDateInvalid, isTaskInvalid} from '../../validation.js';
 import {selectAllProjects} from './project.js';
+import {getFormatedDate} from '../utils.js';
+const clearTerminal = () => {
+  process.stdout.write('\x1Bc'); // This escape character clears the terminal
+};
 
-const createTask = async () => {
+const createTask = async earlyStop => {
   if (!(await projectModel.isProject())) return;
   const title = await inquirer.input({
     message: 'Insert task title',
@@ -45,11 +49,9 @@ const createTask = async () => {
 
   const project = projects.find(c => c.name === name);
 
+  if (earlyStop) return taskService.create({title, projectId: project.id});
   // Get current date and time
-  const now = new Date();
-  const date = now.toISOString().split('T')[0]; // format YYYY-MM-DD
-  const time = now.toTimeString().split(' ')[0].slice(0, 5); // format HH:mm
-  const startDefault = `${date} ${time}`;
+  const startDefault = getFormatedDate();
 
   // Prompt for start and end dates with time
   const start = await inquirer.input({
@@ -78,7 +80,7 @@ const createTask = async () => {
     },
   });
 
-  await taskService.create(start, end, title, project.id);
+  await taskService.create({start, end, title, projectId: project.id});
 };
 
 const editTask = async () => {
@@ -187,7 +189,13 @@ const editTask = async () => {
     },
   });
 
-  await taskService.update(task.id, start, end, title, project.id);
+  await taskService.update({
+    id: task.id,
+    start,
+    end,
+    title,
+    projectId: project.id,
+  });
 };
 
 const selectAllTasks = async () => {
@@ -232,4 +240,81 @@ const deleteTask = async () => {
   return taskService.delete(task.id);
 };
 
-export {createTask, editTask, selectAllTasks, deleteTask};
+const timer = async () => {
+  return new Promise(async (resolve, reject) => {
+    // type task name or select from known
+    const tasks = await taskService.selectAll();
+    // TODO: make unique
+    // // TODO:add project
+    const choices = tasks
+      .map(t => ({name: t.title, value: t.title}))
+      .concat({name: 'Create Task', value: 'new'});
+
+    const choice = await inquirer.search({
+      message: 'Select task',
+      source: input => {
+        if (!input) return choices;
+
+        const sw = input.toLowerCase().replaceAll(' ', '');
+        // TODO: fuzzy finder
+        return choices.filter(choice =>
+          choice.name
+            .toLowerCase()
+            .replace(' ', '')
+            .match(new RegExp(sw, 'gi')),
+        );
+      },
+    });
+    let taskArr;
+    if (choice === 'new') {
+      const [id] = await createTask(true);
+
+      taskArr = await taskService.selectById(id);
+    } else {
+      const {title, project_id} = tasks.find(t => t.title === choice);
+
+      const [id] = await taskService.create({title, projectId: project_id});
+      taskArr = await taskService.selectById(id);
+    }
+
+    const task = taskArr[0];
+
+    const timerId = setInterval(() => {
+      const currTime =
+        Math.floor(new Date().getTime() / 1000) -
+        Math.floor(new Date(task.start).getTime() / 1000);
+      clearTerminal();
+      console.log(currTime);
+    }, 1000);
+
+    process.stdin.resume();
+    process.stdin.setRawMode(true);
+
+    const keypressHandler = async key => {
+      if (key.toString() === 's') {
+        clearInterval(timerId);
+        const time =
+          Math.floor(new Date().getTime() / 1000) -
+          Math.floor(new Date(task.start).getTime() / 1000);
+
+        await taskService.update({
+          id: task.id,
+          end: getFormatedDate(),
+          projectId: task.project_id,
+          time,
+        });
+
+        process.stdin.off('data', keypressHandler);
+        resolve();
+
+        // TODO: verify below lines
+        process.stdin.end();
+        process.stdin.setRawMode(false);
+      }
+    };
+
+    process.stdin.on('data', keypressHandler);
+  });
+};
+
+export {createTask, editTask, selectAllTasks, deleteTask, timer};
