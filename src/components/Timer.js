e2078f92007e394db98de9054453a995e729610d
kamil <kamil.mrowka@outlook.com.au>
2025-11-27 08:59:00 +0100

    Move timer to component dir

---
+++ src/components/Timer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Text} from "ink";
+
+const Clock = () => {
+  const [time, setTime] = React.useState(new Date().toLocaleTimeString());
+  React.useEffect(() => {
+    // create a simle clock here
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <Text color="green">{time}</Text>;
+};
+
+export default Clock;
