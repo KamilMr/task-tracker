db9297ac57f707e83a985c09a2980a30b810cade
kamil <kamil.mrowka@outlook.com.au>
2026-01-08 18:32:52 +0100

    Make full screen true

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@
 import React from 'react';
 import {program} from 'commander';
 
-import {clearTerminal} from './utils.js';
-
 program.version('1.0.0');
 
 import {render} from 'ink';
 import App from './app.js';
 
-clearTerminal();
-render(<App />);
+render(<App />, {fullscreen: true});
